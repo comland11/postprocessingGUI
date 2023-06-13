7c0f08dd38c623da3d0fa1038e35b44f49e7b000
DESKTOP-TQILDKA\Portatil PC 6 <dhareena.comlan@gmail.com>
2023-06-13 12:35:52 +0200

    updates

--- widget/reconstruction_tab_widget.py
+++ widget/reconstruction_tab_widget.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QPushButton, QTabWidget, QVBoxLayout
+from PyQt5.QtWidgets import QPushButton, QTabWidget, QVBoxLayout, QLabel, QLineEdit, QHBoxLayout
 
 
 class ReconstructionTabWidget(QTabWidget):
@@ -6,11 +6,28 @@ class ReconstructionTabWidget(QTabWidget):
         super(ReconstructionTabWidget, self).__init__(*args, **kwargs)
         self.main = parent
 
+        # Buttons
         self.image_fft_button = QPushButton('FFT')
         self.image_art_button = QPushButton('ART')
 
+        # Labels
+        self.niter_label = QLabel('Number of iteration')
+        self.lambda_label = QLabel('Lambda')
+
+        # Text Fields
+        self.niter_text_field = QLineEdit()
+        self.lambda_text_field = QLineEdit()
+
+        # Layouts
+        self.art_layout = QHBoxLayout()
+        self.art_layout.addWidget(self.niter_label)
+        self.art_layout.addWidget(self.niter_text_field)
+        self.art_layout.addWidget(self.lambda_label)
+        self.art_layout.addWidget(self.lambda_text_field)
+
         self.reconstruction_layout = QVBoxLayout()
-        self.reconstruction_layout.addWidget(self.image_fft_button)
+        self.reconstruction_layout.addLayout(self.art_layout)
         self.reconstruction_layout.addWidget(self.image_art_button)
+        self.reconstruction_layout.addWidget(self.image_fft_button)
 
         self.setLayout(self.reconstruction_layout)
